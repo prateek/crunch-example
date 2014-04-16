326b5175a7a5e76ddbc4419153d4565bde3c9774
Prateek Rungta <prungta2@gmail.com>
2014-04-16 01:22:24 -0400

    dataset creation working

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ id, name, age, salary, years_spent, title, department
 ```
 
 ## Example 1: Find average salary per department ##
-This is a facetious example. It is created with to demonstrate the scaffolding required around getting the two frameworks (`Kite` and `Crunch` rolling along). 
+This is a facetious example. It is created with to demonstrate the scaffolding required around getting the two frameworks (`Kite` and `Crunch` rolling along).
 
-The equivalent SQL query for the computation being performed - 
+The equivalent SQL query for the computation being performed -
 
 ```sql
-SELECT 
+SELECT
     department
   , AVERAGE( salary)
-FROM 
+FROM
   table
 GROUP BY
   department
@@ -27,9 +27,34 @@ GROUP BY
 
 
 #### Notes:####
-* Generate the schema classes - 
+* Need to add the lines below to the mvn kite pom to connect to the cluster
+
+```xml
+<hadoopConfiguration>
+  <property>
+    <name>fs.default.name</name>
+    <value>hdfs://localhost</value>
+  </property>
+  <property>
+    <name>hive.metastore.uris</name>
+    <value>thrift://localhost:9083</value>
+  </property>
+</hadoopConfiguration>
+```
+
+* Generate the datasets
+
+```sh
+mvn kite:create-dataset                                         \
+-Dkite.datasetName=employee_records                             \
+-Dkite.avroSchemaFile=src/resources/schema/employee_record.avsc \
+-Dkite.rootDirectory=/tmp/employee_records
+```
+
+* Generate the schema classes -
 ```sh
 java -jar avro-tools-1.7.6.jar compile schema \
   src/resources/schema/employee_summary.avsc src/java
 ```
 
+* FIXME: needed to add `src/main/resources/core-site.xml` to get the Kite tool to pickup HDFS locations
---
+++ src/main/java/com/cloudera/sa/examples/crunch/GenerateSummaries.java
@@ -0,0 +1,99 @@
+package com.cloudera.sa.examples.crunch;
+
+import org.apache.crunch.PCollection;
+import org.apache.crunch.Target;
+import org.apache.crunch.types.avro.Avros;
+import org.apache.crunch.util.CrunchTool;
+import org.apache.hadoop.util.ToolRunner;
+
+import org.kitesdk.data.crunch.CrunchDatasets;
+import org.kitesdk.data.DatasetDescriptor;
+import org.kitesdk.data.Dataset;
+import org.kitesdk.data.DatasetReader;
+import org.kitesdk.data.DatasetRepositories;
+import org.kitesdk.data.DatasetRepository;
+import org.kitesdk.data.Formats;
+
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import com.cloudera.sa.examples.EmployeeRecord;
+
+public class GenerateSummaries extends CrunchTool {
+
+  @Override
+  public int run(String[] args) throws Exception {
+
+    if( args.length != 1 ) {
+      System.out.println("Usage: GenerateSummaries <employee-records-location> ");
+      System.exit( -1 );
+    }
+
+    /* debugging flags */
+    getPipeline().enableDebug();
+    getPipeline().getConfiguration().set( "crunch.log.job.progress", "true" );
+
+    DatasetRepository temp_fs_repo = DatasetRepositories.open( "repo:hdfs:/tmp/crunch-example" );
+
+    if( !temp_fs_repo.exists( "employee_records" ) ) {
+      Schema employee_record_schema = SchemaBuilder.record("EmployeeRecord")
+          .fields()
+          .name("id").type().stringType().noDefault()
+          .name("name").type().stringType().noDefault()
+          .name("age").type().intType().noDefault()
+          .name("salary").type().doubleType().noDefault()
+          .name("years_spent").type().intType().noDefault()
+          .name("title").type().stringType().noDefault()
+          .name("department").type().stringType().noDefault()
+          .endRecord();
+
+      temp_fs_repo.create("employee_records", new DatasetDescriptor.Builder()
+          // FIXME: descriptor doesn't take location as arg yet -- .location( args[0] )
+          .format(Formats.CSV)
+          .property("kite.csv.delimiter", ",")
+          .schema(employee_record_schema)
+          .build());
+    }
+
+    Dataset< EmployeeRecord > employee_records = temp_fs_repo.load( "employee_records" );
+    DatasetReader reader = employee_records.newReader();
+    try {
+      reader.open();
+      for (Object rec : reader) {
+        System.err.println("Movie: " + rec);
+      }
+    } finally {
+      reader.close();
+    }
+
+    return 0;
+  }
+
+  /*
+    Dataset<Summary> summariesDataset = repo.load("summaries");
+
+    PCollection<Event> events = read(
+        CrunchDatasets.asSource(eventsDataset, Event.class));
+
+    PCollection<Summary> summaries = events
+        .by(new GetTimeAndSourceBucket(),
+            Avros.pairs(Avros.longs(), Avros.strings()))
+        .groupByKey()
+        .parallelDo(new MakeSummary(),
+            Avros.reflects(Summary.class));
+
+    getPipeline().write(summaries,
+        CrunchDatasets.asTarget(summariesDataset),
+        Target.WriteMode.APPEND);
+
+    return run().succeeded() ? 0 : 1;
+        */
+
+  public static void main(String[] args) throws Exception {
+    int rc = ToolRunner.run(new GenerateSummaries(), args);
+    System.exit(rc);
+  }
+}
